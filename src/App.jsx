b2b7207750e6bcc2f66962d7d0dbd02240224724
Jeet Patel <pateljenil636@gmail.com>
2022-12-30 18:09:15 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import { Configuration, OpenAIApi } from "openai";
+import "./App.css";
+
+function App() {
+  const [prompt, setPrompt] = useState("");
+  const [result, setResult] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [placeholder, setPlaceholder] = useState(
+    "Search Bears with Paint Brushes the Starry Night, painted by Vincent Van Gogh.."
+  );
+  const configuration = new Configuration({
+    apiKey: import.meta.env.VITE_Open_AI_Key,
+  });
+
+  const openai = new OpenAIApi(configuration);
+
+  const generateImage = async () => {
+    setPlaceholder(`Searched for.... ${prompt}..`);
+    setLoading(true);
+    const res = await openai.createImage({
+      prompt: prompt,
+      n: 1,
+      size: "512x512",
+    });
+    setLoading(false);
+    setResult(res.data.data[0].url);
+  };
+  return (
+    <div className="app-main">
+      {loading ? (
+        <>
+          <h2>Generating..Please Wait..</h2>
+          <div class="lds-ripple">
+            <div></div>
+            <div></div>
+          </div>
+        </>
+      ) : (
+        <>
+          <h2>Generate an Image using Open AI API</h2>
+
+          <textarea
+            value={prompt}
+            className="app-input text-area"
+            placeholder={placeholder}
+            onChange={(e) => setPrompt(e.target.value)}
+            rows="10"
+            cols="40"
+          />
+          <div className="buttons">
+            <button onClick={generateImage}>Generate an Image</button>
+            <button onClick={() => setPrompt("")}>Reset Prompt</button>
+          </div>
+          {result.length > 0 ? (
+            <img className="result-image" src={result} alt="result" />
+          ) : (
+            <></>
+          )}
+        </>
+      )}
+    </div>
+  );
+}
+
+export default App;
